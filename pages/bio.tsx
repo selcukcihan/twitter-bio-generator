f080c540d80d981db92d2bfe7a61cfbdf5a2d43c
Selcuk Cihan <selcukcihan@gmail.com>
2023-03-19 23:22:36 +0300

    change waiting message

--- pages/bio.tsx
+++ pages/bio.tsx
@@ -21,7 +21,7 @@ const getOuterComponent = (isLoaded: boolean, inner: ReactElement) => (
           <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
             <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
             <h2 className="text-center text-slate-300 text-xl font-semibold">Loading...</h2>
-            <p className="w-1/3 text-center text-slate-300">Please wait until user data is ready...</p>
+            <p className="w-1/3 text-center text-slate-300">Generating your bio, this can take a couple of seconds...</p>
           </div>}
         {isLoaded && inner}
       </div>
